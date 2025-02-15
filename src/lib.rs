2979a1e8d000ebf87fbc63b2229e7278626b34f6
Thomas Veness <veness@protonmail.com>
2025-02-15 11:02:07 -0500

    Link README to docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,5 @@
 #![deny(missing_docs)]
-
-//! A simple implementation of a kill switch, using only the standard library
-//!
-//! # Example
-//! ```
-//!# tokio_test::block_on( async {
-//! use std::time::Duration;
-//! use killswitch_std::KillSwitch;
-//!
-//! // Create a kill switch
-//! let kill = KillSwitch::default();
-//!
-//! println!("Is kill switch set? {}", kill);
-//!
-//! // Now make a couple of clones and check
-//! let k1 = kill.watcher();
-//! let t1 = tokio::spawn(async move {
-//!     let duration = Duration::from_secs(2);
-//!     for _ in 0..5 {
-//!         tokio::time::sleep(duration).await;
-//!         println!("Kill switch on thread 1: {}", k1);
-//!     }
-//!     println!("Thread 1 wrapping up");
-//! });
-//!
-//! // Now make a couple of clones and check
-//! let k2 = kill.watcher();
-//! let t2 = tokio::spawn(async move {
-//!     let duration = Duration::from_secs(2);
-//!     for _ in 0..5 {
-//!         tokio::time::sleep(duration).await;
-//!         println!("Kill switch on thread 2: {}", k2);
-//!     }
-//!     println!("Thread 2 wrapping up");
-//! });
-//!
-//! let duration = Duration::from_secs(7);
-//! tokio::time::sleep(duration).await;
-//! println!("Flipping kill switch on main thread");
-//! let _ = kill.kill();
-//!
-//! let _ = tokio::join!(t1, t2);
-//!
-//! println!("All threads finished");
-//!# })
-//! ```
+#![doc = include_str!("../README.md")]
 
 use std::{
     fmt::Display,
